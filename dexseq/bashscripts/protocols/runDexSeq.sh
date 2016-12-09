2194c909e700c07eadbcee3d54e3fca3e652b49a
Lsteenhuis <larssteenhuis@gmail.com>
2016-12-09 13:14:35 +0100

    Only copies files that need to be used now.

--- dexseq/bashscripts/protocols/runDexSeq.sh
+++ dexseq/bashscripts/protocols/runDexSeq.sh
@@ -1,6 +1,7 @@
 ml R
 
-cp ../../counted_files/* $TMPDIR
+cp ../../counted_files/countfiles $TMPDIR
+cp ../../counted_files/rpmifiles $TMPDIR
 cp ../../gff/Hsap.GRCh37.75.gff $TMPDIR
 
 Rscript --vanilla stimuliscript
