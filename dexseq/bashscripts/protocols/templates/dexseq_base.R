8519dcb0af37cc772d52c0533593cc681bad3ccd
Lsteenhuis <larssteenhuis@gmail.com>
2016-12-09 11:35:35 +0100

    dexseq_base now selects the files from slurm tmpdir

--- dexseq/bashscripts/protocols/templates/dexseq_base.R
+++ dexseq/bashscripts/protocols/templates/dexseq_base.R
@@ -1,19 +1,18 @@
 ########################################################################
 #DEXSEQ
-
 library("DEXSeq")
 library("BiocParallel")
 BPPARAM = MulticoreParam(workers=4)
-tmpdir <- Sys.getenv("TMPDIR")
 
-flattenedFile = "tmpdir/Hsap.GRCh37.75.gff"
-countFiles = list.files("tmpdir/", pattern="stimuli_time", full.names = TRUE)
-RPMI = list.files("tmpdir/", pattern="RPMI_time", full.names = TRUE)
+tmpdir <- Sys.getenv("TMPDIR")
+flattenedFile <- list.files(tmpdir, pattern="gff$", full.names = TRUE)
+countFiles = list.files(tmpdir, pattern="Aspergillus_fumigatus_4h", full.names = TRUE)
+RPMI = list.files(tmpdir, pattern="RPMI_4h", full.names = TRUE)
 countFiles <- append(countFiles, RPMI)
 
 sampleTable = data.frame(
   row.names = c(vectorS,
-                vectorR) ,
+                vectorR),
 
   condition = c(stimCon,
                 rpmiCon),
@@ -22,15 +21,25 @@ sampleTable = data.frame(
 
 suppressPackageStartupMessages( library( "DEXSeq" ) )
 dxd = DEXSeqDataSetFromHTSeq(
-  countFiles,
-  sampleData=sampleTable,
-  design= ~ sample + exon + condition:exon,
-  flattenedfile=flattenedFile)
+        countFiles,
+        sampleData=sampleTable,
+        design= ~ sample + exon + condition:exon,
+        flattenedfile=flattenedFile)
 
+print("estimating sizefactors")
 dxdSf = estimateSizeFactors( dxd )
+
+print("estimating dispersions")
 dxdDis = estimateDispersions( dxdSf, BPPARAM=BPPARAM)
+
+print("testing for differential exon usage")
 dxdDEU = testForDEU( dxdDis, BPPARAM=BPPARAM)
+
+print("estimating exon fold changes")
 dxdFC <- estimateExonFoldChanges( dxdDEU, BPPARAM=BPPARAM)
+
+print("creating results object")
 dxr = DEXSeqResults( dxdFC )
+
+print("saving results object")
 save(dxr,file = "../../dexseq_results/results/time/stimuli.results")
-save(dxdFC, file =  "../../dexseq_results/foldingChanges/time/stimuli.dxdFC")
