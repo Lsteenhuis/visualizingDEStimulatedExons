5854624a9ccb1a1880f02ea6a63491109ebfffd7
Lsteenhuis <larssteenhuis@gmail.com>
2017-01-27 15:35:31 +0100

    Pipeline template for the comparison of MISO output

---
+++ miso/scripts/bashscripts/protocols/compareMisoOutput.sh
@@ -0,0 +1,21 @@
+# START OF PROTOCOL
+
+module load Python/2.7.9-foss-2015b
+module load Pysam/0.9.0-foss-2015b-Python-2.7.9
+module load matplotlib/1.5.1-foss-2015b-Python-2.7.9
+module load scipy/0.15.1-foss-2015b-Python-2.7.9
+module load numpy/1.9.2-foss-2015b-Python-2.7.9
+module load BEDTools
+module load SAMtools
+export PYTHONPATH="${PYTHONPATH}:/groups/umcg-wijmenga/tmp04/umcg-lsteenhuis/python/lib"
+
+#create .started file
+touch scriptName.started
+
+# MISO compare control group with a stimulus
+python /groups/umcg-wijmenga/tmp04/umcg-lsteenhuis/python/lib/misopy/compare_miso.py --compare-samples \
+	RPMI \
+	stimuli \
+	comparisonOutput
+
+# END OF PROTOCOL
